a2f12dba25ca86fcf803ba49944be3f496da7261
nastassia <anastasiyamihalenko@gmail.com>
2021-10-05 17:09:09 +0300

    6

--- index.js
+++ index.js
@@ -118,3 +118,12 @@ function isIsogram(str){
   return true;
 }
 
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+
+
+function solution(str){
+    return str.split('').reverse().join('')
+  }
