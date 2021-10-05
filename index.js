3b3d5409aaa4ce4cdfb0994418646e9c6f400c74
nastassia <anastasiyamihalenko@gmail.com>
2021-10-05 17:02:44 +0300

    3

--- index.js
+++ index.js
@@ -50,3 +50,28 @@ function squaresNeeded(grains){
     
   }
 
+
+//   To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+// The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+// Example:
+
+// multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+// You must not mutate the original array.
+
+
+function multiplyAll(arr) {
+    return function(n) {
+      if (arr.length == 0){
+        return []; 
+        } else {
+          let result = []
+        for (let i = 0; i < arr.length; i++){
+          result.push(arr[i]*n)
+        }
+          return result
+      }
+    }
+  }
+
