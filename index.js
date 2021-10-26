af981b78eee9f0ba726ea49ab988f4ab6e77f0ba
nastassia <anastasiyamihalenko@gmail.com>
2021-10-26 20:01:30 +0300

    add

--- index.js
+++ index.js
@@ -127,3 +127,20 @@ function isIsogram(str){
 function solution(str){
     return str.split('').reverse().join('')
   }
+
+//   Your job is to figure out the index of which vowel is missing from a given string:
+// A has an index of 0,
+// E has an index of 1,
+// I has an index of 2,
+// O has an index of 3,
+// U has an index of 4.
+// Notes: There is no need for string validation and every sentence given will contain all vowels but one. Also, you won't need to worry about capitals.
+
+function absentVowel(x){
+  let vowels = 'aeiou';
+for (let i in vowels) {
+  if (x.toLowerCase().indexOf(vowels[i]) == -1) {
+    return Number(i);
+  }
+}
+}
