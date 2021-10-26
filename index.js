676aa1c66619c399caca96f71d53a001c28fadcc
nastassia <anastasiyamihalenko@gmail.com>
2021-10-26 20:24:45 +0300

    add

--- index.js
+++ index.js
@@ -152,4 +152,31 @@ function usdcny(usd) {
 }
 
 
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
 
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+// For example, decode("h3 th2r2") would return "hi there".
+
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+
+function encode(string) {
+  const vowels = ['a', 'e' ,'i', 'o', 'u'];
+  const vowelsNum = ['1', '2', '3', '4', '5']; 
+  let encode = [...string].map(e => vowels.includes(e) ? e = vowels.indexOf(e) + 1 : e).join('');
+  return encode
+}
+
+function decode(string) {
+  const vowels = ['a', 'e' ,'i', 'o', 'u'];
+  const vowelsNum = ['1', '2', '3', '4', '5']; 
+  let decode = [...string].map(e => vowelsNum.includes(e) ? e = vowels[vowelsNum.indexOf(e)] : e).join('');
+  return decode
+}
