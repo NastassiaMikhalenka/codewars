e40ae05b8a17f30e2b5f35a7d1f096b19805b76a
nastassia <anastasiyamihalenko@gmail.com>
2021-10-28 23:03:37 +0300

    add task

--- index.js
+++ index.js
@@ -223,3 +223,13 @@ function multiplicationTable(row, col){
  }
  return array;
 }
+
+// Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+// kebabize('camelsHaveThreeHumps') // camels-have-three-humps
+// kebabize('camelsHave3Humps') // camels-have-humps
+
+function kebabize(str) {  
+  return str.replace(/[0-9]/g, '').split(/(?=[A-Z])/).join('-').toLowerCase() 
+}
+
