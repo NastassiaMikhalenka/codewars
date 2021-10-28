1644869db1ace54ef4b3760fb24c7512a6aaeb37
nastassia <anastasiyamihalenko@gmail.com>
2021-10-29 01:22:55 +0300

    add

--- index.js
+++ index.js
@@ -253,3 +253,19 @@ function high(x){
 function stringToArray(string){
   return string.split(" ")
 }
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+
+function check(a, x) {
+  let str = a.indexOf(x)
+  if(str !== -1){
+    return true
+  } else {
+    return false
+  }
+  }
