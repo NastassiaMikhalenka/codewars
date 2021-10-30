fa080e47f88e0c0664e4b9d0f2f156eb91a0038e
nastassia <anastasiyamihalenko@gmail.com>
2021-10-30 12:41:29 +0300

    add

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -462,4 +462,9 @@ function likes(names) {
   if (names.length === 2) return names[0] + " and " + names[1] + " like this";
   if (names.length === 3) return names[0] + ", " + names[1] + " and " + names[2] + " like this";
   return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+} // String ends with?
+
+
+function solutions(str, ending) {
+  return str.endsWith(ending);
 }
